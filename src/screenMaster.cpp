bddbeac289c0dcee15d96dab19e73103b5888f0e
dosisod <39638017+dosisod@users.noreply.github.com>
2019-06-15 16:16:17 -0700

    main now uses screen master

--- src/screenMaster.cpp
+++ src/screenMaster.cpp
@@ -1,7 +1,12 @@
 #include "screenMaster.hpp"
 
+ScreenMaster::ScreenMaster(std::shared_ptr<Terminal> t) {
+	term=t;
+}
+
 void ScreenMaster::addBuffer(std::string s) {
 	screens.emplace_back(
-		std::make_shared<Screen>()
+		std::make_shared<Screen>(term)
 	);
+	screens[screens.size()-1]->useBuffer(std::make_shared<File>(s));
 }
--- src/screenMaster.hpp
+++ src/screenMaster.hpp
@@ -9,14 +9,20 @@
 
 class ScreenMaster {
 public:
-	ScreenMaster();
+	ScreenMaster(std::shared_ptr<Terminal> t);
 
 	void addBuffer(std::string s); //adds a new screen buffer using file buffer initializer
+
+	//all -> functions done to master are passed to current screen
+	std::shared_ptr<Screen> operator->() {
+		return screens[currentscr];
+	}
 
 private:
 	std::shared_ptr<Terminal> term;
 
 	std::vector<std::shared_ptr<Screen>> screens;
+	int currentscr=0; //holds id of current screen
 };
 
 #endif
